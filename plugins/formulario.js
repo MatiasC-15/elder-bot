ea43dda36fe749d6e51367c9392cf9e2bb734fe3
MatiasC-15 <kenddrysilva15@gmail.com>
2025-01-08 21:58:50 -0500

    Create formulario.js

---
+++ plugins/formulario.js
@@ -0,0 +1,20 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.SpamLink);
+};
+handler.command = /^(formulario)$/i;
+export default handler;
+global.SpamLink = `
+
+LLENAR FORMULARIO
+
+𝑵𝑰𝑪𝑲 𝑭𝑭:
+𝑵𝑶𝑴𝑩𝑹𝑬:
+𝑬𝑫𝑨𝑫:
+𝑹𝑶𝑳𝑳:
+𝑹𝑬𝑮𝑰𝑶𝑵:
+𝑫𝑰𝑺𝑷𝑶𝑵𝑰𝑩𝑰𝑳𝑰𝑫𝑨𝑫:
+𝑪𝑨𝑴𝑩𝑰𝑶 𝑫 𝑵𝑰𝑪𝑲:
+𝑴𝑶𝑽𝑰𝑳 𝑶 𝑷𝑪:
+
+𝑵𝑶𝑻𝑨: recuerda una vez hayas pasado el formulario se te hará unas pruebas en Vv2 y scrims.
+`;
